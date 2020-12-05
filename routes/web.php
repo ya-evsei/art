cd5d7e2045a726cbfbd177077d0feb5f508a79f0
Ya-evsei <ya.evsei@gmail.com>
2020-12-05 21:02:55 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\MKController;
+use App\Http\Controllers\MkImageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MkController::class, 'allImg']);
+Route::get('mk-img', [ MkImageController::class, 'index' ]);
+Route::post('mk-img', [ MkImageController::class, 'store' ])->name('images.store');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
